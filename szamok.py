ef4af40991311b2c131b12d4473918936a43568f
Training <andr3w.89@gmail.com>
2021-10-13 14:09:59 +0200

    első verzió

---
+++ szamok.py
@@ -0,0 +1,12 @@
+# Ez a program összead
+
+a = int(input("Add meg az első számot: ")) # ez bekéri a számot
+b = int(input("Add meg a második számot: "))
+print(a + b)
+
+a_input = int(input("Add meg az első számot: "))
+b_input = int(input("Add meg a második számot: "))
+a = int(a_input)
+b = int(b_input)
+print(a + b) # ez iratja ki a végeredményt
+
